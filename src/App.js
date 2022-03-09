7776a424ffd6b0b7e401a2b6f6458864266fbd8b
Jairus Ser Aragon <jairus.aragon@zuhlke.com>
2022-03-10 00:12:04 +0800

    fix for penup/pendown

--- src/App.js
+++ src/App.js
@@ -3,69 +3,49 @@ import Turtle from 'react-turtle'
 import {produce} from './FractalTree'
 
 function App() {
-
     const command = produce('0', 5);
-
-    // const command = '1[0]1'
     console.log('command:', command);
 
     return (
         <div className="App">
-            <Turtle width={1000} height={2000} draw={(turtle) => {
+            <Turtle width={500} height={500} draw={(turtle) => {
+                turtle.penup();
+                turtle.setPosition(0,250);
+                turtle.pendown();
                 turtle.left(90);
                 const stack = [];
-                command.split('').map(c => {
-                    console.log({
-                        x: turtle.x,
-                        y: turtle.y,
-                        heading: turtle.heading
-                    });
+                command.split('').forEach(c => {
                     switch (c) {
                         case '0':
-                            console.log('0');
                             turtle.forward(8);
                             turtle.stroke();
                             break;
                         case '1':
-                            console.log('1')
                             turtle.forward(16);
                             turtle.stroke();
                             break;
                         case '[':
-                            console.log('[');
                             const state = {
                                 x: turtle.x,
                                 y: turtle.y,
                                 heading: turtle.heading
                             };
                             stack.push(state);
-                            // turtle.save();
                             turtle.left(45);
                             break;
                         case ']':
+                            turtle.penup();
                             const { x, y, heading} = stack.pop();
-                            console.log('] begin', x, y, heading)
-                            //turtle.penup()
                             turtle.setPosition(x, y);
-                            //turtle.pendown();
+                            turtle.pendown();
+                            turtle.forward(0); // hack to pendown
                             turtle.setheading(heading);
-                            // turtle.pendown()
                             turtle.right(45);
-                            console.log('] end', {
-                                x: turtle.x,
-                                y: turtle.y,
-                                heading: turtle.heading
-                            });
+                            break;
+                        default:
                             break;
                     }
                 });
-                console.log({
-                    x: turtle.x,
-                    y: turtle.y,
-                    heading: turtle.heading
-                });
-
-                // turtle.stroke();
             }}/>
         </div>
     );
